1288148b4282d3a7c19e6007efb286b666a01d50
Nicoleta <simion.raluca29@yahoo.com>
2020-07-11 15:37:06 +0300

    folder

--- script/main.js
+++ script/main.js
@@ -2,44 +2,38 @@ const {
     parseWithoutProcessing
 } = require("handlebars");
 
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', () => {
 
-    $('.dropdown').click(function () {
-        $(this).siblings(".submenu").toggleClass('hide');
-    });
-}
-document.addEventListener("DOMContentLoaded", () => {
-    //creating a new username
-    if (document.querySelector("#formDisplay")) {
-        document.querySelector("#formDisplay").onsubmit = () => {
-            //Create new AJAX request
+    if (document.querySelector('#formDisplay')) {
+
+        document.querySelector('#formDisplay').onsubmit = () => {
+            // Create a new AJAX request
             let request = new XMLHttpRequest();
 
-            //Assign value to submit
+            // Asign value to submit
             let name = document.querySelector('#name').value;
             let data = new FormData();
             data.append('name', name);
 
-            //Open request and send
-            request.open("POST", "/name");
+            // Open the the already created request and send
+            request.open('POST', '/name');
 
             request.send(data)
-            //response
+            // Response
             request.onload = () => {
                 let data = JSON.parse(request.responseText);
                 if (data.success) {
-                    document.querySelector("#form").style.dysplay = "none";
-                    let h = document.createElement("h3");
-                    h.innerHTML = "Welcome," + data.name + "!";
-                    document.querySelector("#user-data").dataset.username = data.name;
-                    document.querySelector("#DisplayName").append(h);
-                    document.querySelector("#channelContainer").style.display = " ";
+                    document.querySelector('#form').style.display = 'none';
+                    let h = document.createElement('h3');
+                    h.innerHTML = 'Welcome ' + data.name + '!';
+                    document.querySelector('#user-data').dataset.username = data.name;
+                    document.querySelector('#DisplayName').append(h);
+                    document.querySelector('#channelContainer').style.display = '';
                 } else {
-                    document.querySelector("#DisplayName").innerHTML = "Unfortunately there was an error.. Please try again";
+                    document.querySelector('#DisplayName').innerHTML = 'Humm!, Unfortunately there was an error.. Please try again';
                 }
             }
             return false;
         }
     }
-
 });
